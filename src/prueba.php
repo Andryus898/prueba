60a5ab34f9c448e18b5c68862a568af574131e14
Andryus898 <andres@formavclucion.es>
2019-04-04 18:21:18 +0200

    as

--- src/prueba.php
+++ src/prueba.php
@@ -2,7 +2,7 @@
 $enlace=mysqli_connect("localhost","newralers","aA4404","");
 
 if (!$enlace) {
-    die('No pudo conecte: ' . mysql_error());
+    die('No pudo conectarse: ' . mysql_error());
 }
 echo 'Conectado satisfactoriamente';
 mysql_close($enlace);
